06ce20776eff6ec20e12e8618574a94d9e813685
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-06-23 00:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ function updateTimer() {
+      const now = new Date();
+      const timerElement = document.getElementById("timer");
+      timerElement.textContent = now.toLocaleString();
+    }
+
+    // Update the timer every second
+    setInterval(updateTimer, 1000);
